55ca3c160b3af2a11bc0f790ca203089a723c0bd
Sithira Senanayake <sithirasenanayake@gmail.com>
2026-07-27 12:11:54 +0530

    doc: Improve readme file

--- sql-schema.sql
+++ sql-schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE countries (
 	uuid VARCHAR(50) UNIQUE NOT NULL,
-	name VARCHAR(100), 
-	continent VARCHAR(100)
-	sync_level INT
+	name VARCHAR(100) NOT NULL,
+	continent VARCHAR(100),
+	sync_level INT NOT NULL
 )
